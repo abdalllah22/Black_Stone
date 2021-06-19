faeed634b8b83fa15b73e5a15416ece02943ecd2
abdalllah22 <ahmed.abdalllah22@gmail.com>
2021-06-19 02:50:29 +0200

    car passes through the gate 2 times within 1 minute

--- routes/gates.js
+++ routes/gates.js
@@ -31,16 +31,44 @@ router.post('/', async (req,res)=>{
         car: req.body.car,
         dateOfPass: Date.now(),
     })
-   
-    let x = await Car.findById(passgate.car._id);
-    await Car.findByIdAndUpdate(
-        passgate.car._id,
-        {
-            card : x.card - x.priceOfPass
-        }
-    );
-    
+
     passgate = await passgate.save();
+    
+    
+    const passgatelatest = await Gate.find({car: passgate.car._id}).sort({ _id: -1 }).limit(2)
+    
+    let dateOfPassArray = passgatelatest.map(a => a.dateOfPass);
+    dateOfPassArray.sort(function(a, b) {
+        var dateA = new Date(a.release), dateB = new Date(b.release);
+        return dateA - dateB;
+    });
+    
+    let resultInMinutes = Math.round((dateOfPassArray[0] - dateOfPassArray[1]) / 60000);
+    console.log(dateOfPassArray[1])
+    console.log(dateOfPassArray[0])
+    console.log(resultInMinutes)
+
+    if(resultInMinutes === 0){
+        let x = await Car.findById(passgate.car._id);
+        await Car.findByIdAndUpdate(
+            passgate.car._id,
+            {
+                card : x.card 
+            }
+        );
+    }else{
+        let x = await Car.findById(passgate.car._id);
+        await Car.findByIdAndUpdate(
+            passgate.car._id,
+            {
+                card : x.card - x.priceOfPass
+            }
+        );
+    }
+    
+    
+    
+    
     if (!passgate) {
         return res.status(404).send("the pass from  gate cannot be created!");
     }
